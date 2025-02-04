b7be339192a0351497f9807ba40d0ea0f30ddd00
leeeydia <leeeydia@gmail.com>
2025-02-04 16:40:10 +0900

    Node.js 기초초

---
+++ section02/chapter14.js
@@ -0,0 +1,7 @@
+// async 
+// 어떤 함수를 비동기 함수로 만들어주는 키워드
+// 함수가 프로미스를 반환하도록 반환해주는 그런 키워드
+
+async function getData() {
+  return new Promise {{resolve, reject}=>}
+}
---
+++ section03/src/index.js
@@ -0,0 +1,7 @@
+import mul, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+// const { add, sub } = require("./math");
+// console.log("안녕 Node,js");
