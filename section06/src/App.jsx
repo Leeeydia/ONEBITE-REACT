5e0a9b565faf968dd0940140cf9704c557fa2c8c
leeeydia <leeeydia@gmail.com>
2025-02-10 23:10:38 +0900

    카운터 앱 완성성

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -4,9 +4,9 @@ import Viewer from "./components/Viewer";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState[0];
+  const [count, setCount] = useState(0);
   const onClickButton = (value) => {
-    setCount(count+value)
+    setCount(count + value);
   };
   return (
     <div className="App">
