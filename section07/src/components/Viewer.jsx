c04dfdb9357338f5666988c429953ec6a53c1996
leeeydia <leeeydia@gmail.com>
2025-02-11 00:07:03 +0900

    useEffect 기초

---
+++ section07/src/components/Viewer.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+const Viewer = ({ count }) => {
+  return (
+    <div>
+      <div></div>현재 카운트
+      <h1>{count}</h1>
+    </div>
+  );
+};
+
+export default Viewer;
