7478a003a4405e5abd7bb562047c87b74ccf90c5
leeeydia <leeeydia@gmail.com>
2025-02-11 00:21:30 +0900

    리액트 사이클

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,15 +1,29 @@
 import "./App.css";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
 import Viewer from "./components/Viewer";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  const isMount = useRef(false);
+  // 1. 마운트 : 탄생 => 컴포넌트가 마운트 되었을 때만 최초로 한번 실행시킴 (빈배열 사용)
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+  // 2. 업데이트 : 변화, 리렌더링 => 마운트 될 때 한번 실행 된 다음 리렌더링 (업데이트) 될 때마다 계속 실행 (빈배열 사용 x)
 
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
+
   const onClickButton = (value) => {
     setCount(count + value);
   };
@@ -23,6 +37,10 @@ function App() {
             setInput(e.target.value);
           }}
         ></input>
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
