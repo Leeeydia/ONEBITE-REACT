9ce01cbdfa7091bc29fadcd0ae1791ed6bea8665
leeeydia <leeeydia@gmail.com>
2025-02-27 22:57:40 +0900

    2.27 공부

---
+++ section08/src/components/TodoItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoItem = () => {
+  <div>
+    <input type="checkbox"></input>
+    <div>Todo...</div>
+    <div>Date</div>
+    <div>삭제</div>
+  </div>;
+};
+
+export default TodoItem;
