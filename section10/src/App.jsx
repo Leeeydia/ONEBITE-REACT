30d2b384889615497bf82dbaaa2c8cde50afba11
leeeydia <leeeydia@gmail.com>
2025-06-10 17:30:37 +0900

    useCallback

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useReducer } from "react";
+import { useState, useRef, useReducer, useCallback } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
@@ -44,7 +44,7 @@ const App = () => {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -54,22 +54,21 @@ const App = () => {
         date: new Date().getTime(),
       },
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  };
-  // todos.map((todo)=> todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo)
+  }, []);
   return (
     <div className="App">
       TodoList
