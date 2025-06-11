2ae68f2e547fb251113a4319c8b93e936f7fb346
leeeydia <leeeydia@gmail.com>
2025-06-11 14:53:08 +0900

    props drilling

---
+++ section11/src/App.jsx
@@ -0,0 +1,93 @@
+import {
+  useState,
+  useRef,
+  useReducer,
+  useCallback,
+  createContext,
+} from "react";
+import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: true,
+    content: "명구기랑 놀기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: " 삼겹살 먹기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "딩굴딩굴",
+    date: new Date().getTime(),
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case `CREATE`:
+      return [action.data, ...state];
+    case `UPDATE`:
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case `DELETE`:
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+};
+
+export const TodoContext = createContext();
+
+const App = () => {
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = useCallback((content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  }, []);
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      TodoList
+      <Header />
+      <TodoContext.Provider value={{ todos, onCreate, onUpdate, onDelete }}>
+        <Editor />
+        <List />
+      </TodoContext.Provider>
+    </div>
+  );
+};
+
+export default App;
